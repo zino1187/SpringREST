35cb7bd189b38b2c6f54f980a56dd142c06db59f
zino1187 <zino1187@gmail.com>
2019-04-16 18:04:48 +0900

    no message

--- src/com/itbank/controller/rest/MountainController.java
+++ src/com/itbank/controller/rest/MountainController.java
@@ -1,5 +1,15 @@
 package com.itbank.controller.rest;
 
-public class MountainController {
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+public class MountainController {
+	
+	@RequestMapping(value="", method=RequestMethod.GET)
+	public String test() {
+		System.out.println("웹서비스로 목록 요청했네요");
+		return null;
+	}
 }
